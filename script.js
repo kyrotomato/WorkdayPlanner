669f4d50ca0845dbb164370c42bfde8b44d30cb0
kyrotomato <97704480+kyrotomato@users.noreply.github.com>
2022-03-12 13:53:15 -0600

    added scructure of time blocks, and current date display

---
+++ script.js
@@ -0,0 +1,15 @@
+var todaysDate = moment().format("MMM Do YYYY");
+var timeArray = ["8","9","10","11","12","1","2","3","4","5"];
+$(currentDay).text(todaysDate);
+
+//check time
+function checkTime()
+var currentTime = moment().hour();
+
+
+
+
+
+//give save button functionality
+    //save the entered text to local storage
+        //save it at the time entered
